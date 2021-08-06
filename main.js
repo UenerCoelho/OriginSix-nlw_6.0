fb395a8668b1cb2fb546e42163f04610ce163f34
Uener Coelho <uenercoelho2@hotmail.com>
2021-08-06 00:01:18 -0400

    4/8/21 2h of Learning

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('Alert')
+/* Abre e fecha o menu quando clicar nos ícones: hambúrguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Mudar o header da pagina quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // Se o Scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // Se o Scroll é menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
